3acab19f1c062a86d6920f6d9e85910b730ac112
Jayant <jayant.ojha@sanskrut.com>
2019-09-09 01:59:09 +0530

    Recipe LIst Component Baisc Design

--- Shopping-app/src/app/recipies/recipie-list/recipie-list.component.ts
+++ Shopping-app/src/app/recipies/recipie-list/recipie-list.component.ts
@@ -1,13 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipie } from '../recipie.model';
 
 @Component({
   selector: 'app-recipie-list',
   templateUrl: './recipie-list.component.html',
   styleUrls: ['./recipie-list.component.scss']
 })
-export class RecipieListComponent implements OnInit {
 
-  constructor() { }
+export class RecipieListComponent implements OnInit {
+  recipies: Recipie[] = [
+    new Recipie('A Test Recipie', 'New Recipie', 'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2018/5/14/1/WU1902_Italian-Chicken-Sheet-Pan-Supper_s4x3.jpg.rend.hgtvcom.826.620.suffix/1526332485385.jpeg')
+  ];
+
+constructor() { }
 
   ngOnInit() {
   }
